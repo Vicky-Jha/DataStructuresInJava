6c502e34ded79d64b35f1425735be84836f4d1bf
Vicky-Jha <53377467+Vicky-Jha@users.noreply.github.com>
2021-07-12 08:24:12 +0530

    Update and rename Infix.java to InfixEvaluation.java

--- InfixEvaluation.java
+++ InfixEvaluation.java
@@ -160,7 +160,7 @@ class infixEvaluation {
     }
 }
 
-public class Infix {
+public class InfixEvaluation {
 
     public static void main(String[] args) {
         System.out.println("Enter the correct Expression");
